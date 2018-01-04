9371a2782ae34155733c9775b7aa3585cef39fc3
Sarabjit Bagga <sbagga2@sapient.com>
2018-01-05 00:57:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/sarabblog/smsauthenticationdemo/SmsTokenResultVerificationActivity.java
@@ -0,0 +1,13 @@
+package com.sarabblog.smsauthenticationdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmsTokenResultVerificationActivity extends Activity {
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_sms_token_result);
+    }
+}
